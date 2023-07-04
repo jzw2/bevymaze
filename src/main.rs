f7b153f04584bdd3a14d74fd5ee9368a7c8fa87f
Krist Pregracke <krist@tiger-scm.com>
2023-07-04 10:45:30 -0500

    fix semicolon

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
             .add_startup_system(add_people)
-            .add_system(greet_people)
+            .add_system(greet_people);
     }
 }
 
