796bb5348659c4e247646c1bb20347d201574bc0
John Wang <johnz.wang@outlook.com>
2023-10-24 14:03:33 +0200

    Add tungestite and tutorial files

---
+++ server/src/bin/echo-server-copy.rs
@@ -0,0 +1,15 @@
+use tokio::io;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6142").await?;
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            // Copy data here
+        });
+    }
+}
